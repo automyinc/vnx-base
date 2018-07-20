1fbe95767fcd76e623a1408dde4a01959c09272b
Max Wittal <automyinc@gmail.com>
2018-07-20 13:12:11 -0700

    import libvnx_base.so

--- import.sh
+++ import.sh
@@ -8,6 +8,7 @@ rsync -av --delete $SRC/include/vnx/ include/vnx/
 rsync -av --delete $SRC/interface/ interface/vnx/
 rsync -av --delete $SRC/modules/ modules/
 
+cp $SRC/build/libvnx_base* $ARCH/lib/
 cp $SRC/build/tools/vnxclose $ARCH/bin/
 cp $SRC/build/tools/vnxdump $ARCH/bin/
 cp $SRC/build/tools/vnxread $ARCH/bin/
