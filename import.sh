e34d76434ce47af80c3a74fa08c1dfadea628cfe
Max Wittal <automyinc@gmail.com>
2018-09-06 12:08:20 +0200

    new packaging system

--- import.sh
+++ import.sh
@@ -1,25 +1,22 @@
 #!/bin/bash
 
 SRC=$1
-ARCH=$(uname -m)
-DIST=$(cat ~/DIST)
 
-mkdir -p $ARCH/$DIST/lib/
-mkdir -p $ARCH/$DIST/bin/
-
-cp $SRC/VERSION VERSION
+mkdir -p lib/
+mkdir -p bin/
 
 rsync -av --delete $SRC/generated/include/vnx/ generated/include/vnx/
 rsync -av --delete $SRC/include/vnx/ include/vnx/
 rsync -av --delete $SRC/interface/ interface/vnx/
 rsync -av --delete $SRC/modules/ modules/
 
+cp $SRC/VERSION VERSION
 cp $SRC/doxygen.cfg ./
-cp $SRC/build/libvnx_base* $ARCH/$DIST/lib/
-cp $SRC/build/tools/vnxclose $ARCH/$DIST/bin/
-cp $SRC/build/tools/vnxdump $ARCH/$DIST/bin/
-cp $SRC/build/tools/vnxread $ARCH/$DIST/bin/
-cp $SRC/build/tools/vnxrouter $ARCH/$DIST/bin/
-cp $SRC/build/tools/vnxtimeserver $ARCH/$DIST/bin/
-cp $SRC/build/tools/vnxtopic $ARCH/$DIST/bin/
+cp $SRC/build/libvnx_base* lib/
+cp $SRC/build/tools/vnxclose bin/
+cp $SRC/build/tools/vnxdump bin/
+cp $SRC/build/tools/vnxread bin/
+cp $SRC/build/tools/vnxrouter bin/
+cp $SRC/build/tools/vnxtimeserver bin/
+cp $SRC/build/tools/vnxtopic bin/
 
